e2346144d910ffe38c64320ed7540f1fc5f08aa9
aditi0104 <41106879+aditi0104@users.noreply.github.com>
2023-07-06 16:17:21 -0400

    Update streamlit_app.py
    checking for control of flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,5 @@ streamlit.dataframe(my_data_rows)
 #Allow the end user to add a fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
