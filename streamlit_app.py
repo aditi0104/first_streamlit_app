c5df7e7b351952619582a821fd61eadb2972ffe5
aditi0104 <41106879+aditi0104@users.noreply.github.com>
2023-07-06 16:28:50 -0400

    Update streamlit_app.py
    Added try and catch box allowing to separate the code which needs to be loaded once and which needs to be loaded again and again

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 import pandas as pd
 import snowflake.connector
+import requests
 from urllib.error import URLError
+
+
 streamlit.title('My Mom\'s Healthy New Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -22,17 +25,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# Normalises the json version of the response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output on screen as table
-streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 
